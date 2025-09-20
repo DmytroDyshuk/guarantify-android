7b549326352ea9af6dbb5eb21d2949feaafb3e0a
Dmytro Dyshuk <dd.kronos@gmail.com>
2025-09-20 20:24:31 +0300

    feat(settings-screen): add icons for SettingsScreen [FEAT-19]

--- feature/settings/src/main/java/com/guarantify/settings/SettingsScreen.kt
+++ feature/settings/src/main/java/com/guarantify/settings/SettingsScreen.kt
@@ -1,9 +1,15 @@
 package com.guarantify.settings
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 
 @Composable
 fun SettingsScreen() {
@@ -12,5 +18,31 @@ fun SettingsScreen() {
             .fillMaxSize()
     ) {
 
+    }
+}
+
+@Composable
+fun SettingButton(
+    headlineText: String,
+    leadingIcon: ImageVector,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Icon(
+            imageVector = leadingIcon,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier
+                .weight(1f),
+            text = headlineText
+        )
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.rounded_arrow_right_24),
+            contentDescription = null
+        )
     }
 }
