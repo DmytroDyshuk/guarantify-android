5701253fa25a78c5355a325b1fea6cfb31cdd920
Dmytro Dyshuk <dd.kronos@gmail.com>
2025-10-02 10:18:51 +0300

    fix(home-screen): update colors on home TopAppBar

--- feature/home-navigation/src/main/java/com/guarantify/home_navigation/HomeScreenContainer.kt
+++ feature/home-navigation/src/main/java/com/guarantify/home_navigation/HomeScreenContainer.kt
@@ -12,8 +12,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -26,6 +24,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
 import com.guarantify.home_navigation.bottomnavigation.NavigationBottomBar
+import com.guarantify.home_navigation.components.HomeTopAppBar
 import com.guarantify.insights.InsightsScreen
 import com.guarantify.settings.SettingsScreen
 import com.guarantify.warranties.navigation.WarrantiesDestinations
@@ -43,20 +42,20 @@ fun HomeScreenContainer() {
             currentDestination?.let {
                 when {
                     it.hasRoute<WarrantiesDestinations.WarrantiesScreen>() -> {
-                        TopAppBar(
-                            title = { Text(text = stringResource(R.string.warranties_topbar_title)) }
+                        HomeTopAppBar(
+                            title = stringResource(R.string.warranties_topbar_title)
                         )
                     }
 
                     it.hasRoute<HomeDestinations.Insights>() -> {
-                        TopAppBar(
-                            title = { Text(text = stringResource(R.string.insights_topbar_title)) }
+                        HomeTopAppBar(
+                            title = stringResource(R.string.insights_topbar_title)
                         )
                     }
 
                     it.hasRoute<HomeDestinations.Settings>() -> {
-                        TopAppBar(
-                            title = { Text(text = stringResource(R.string.settings_topbar_title)) }
+                        HomeTopAppBar(
+                            title = stringResource(R.string.settings_topbar_title)
                         )
                     }
                 }
