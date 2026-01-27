6c320ee6ff6ff8c18a32b3dfc9b5161d2cc39df4
Dmytro Dyshuk <dd.kronos@gmail.com>
2026-01-27 03:38:08 +0200

    feat(warranty-details): update WarrantyDetails base layout UI [FEAT-32]
    - add Details UI section
    - add Dates UI section
    - add Attachments UI section
    - add Notes UI section
    - add new icons

--- feature/warranties/src/main/java/com/guarantify/warranties/details/WarrantyDetails.kt
+++ feature/warranties/src/main/java/com/guarantify/warranties/details/WarrantyDetails.kt
@@ -1,6 +1,8 @@
 package com.guarantify.warranties.details
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,16 +20,17 @@ import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.guarantify.ui.R
+import com.guarantify.ui.components.PaddingHorizontalDivider
 import com.guarantify.ui.theme.darkGrayishCyan
 import com.guarantify.ui.theme.emeraldGreen
 import com.guarantify.ui.theme.lavenderGray
@@ -43,16 +46,16 @@ fun WarrantyDetailsScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WarrantyDetailsScreenContent() {
-    val cardInnerHorizontalPadding = 26.dp
+    val defaultInnerHorizontalPadding = 16.dp
 
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
-                    Text("Warranty details") //TODO: display warranty name/brand
+                    Text("Warranty details")
                 },
                 navigationIcon = {
-                    IconButton(onClick = { /* todo: implement back */ }) {
+                    IconButton(onClick = { /* todo: implement back nav */ }) {
                         Icon(
                             imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_back),
                             contentDescription = "Back"
@@ -69,32 +72,27 @@ fun WarrantyDetailsScreenContent() {
                 .padding(horizontal = 12.dp, vertical = 16.dp)
         ) {
             ElevatedCard(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                elevation = CardDefaults.cardElevation(
-                    defaultElevation = 4.dp
-                ),
-                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerHigh)
+                modifier = Modifier.fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainerHigh
+                )
             ) {
                 Text(
                     modifier = Modifier
-                        .padding(horizontal = cardInnerHorizontalPadding)
+                        .padding(horizontal = defaultInnerHorizontalPadding)
                         .padding(top = 12.dp),
                     text = "MacBook Air M1",
                     style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold)
                 )
                 Text(
-                    modifier = Modifier.padding(horizontal = cardInnerHorizontalPadding),
-                    text = buildAnnotatedString {
-                        append("Brand")
-                        append(" · ")
-                        append("Store")
-                    },
+                    modifier = Modifier.padding(horizontal = defaultInnerHorizontalPadding),
+                    text = "Brand",
                     style = MaterialTheme.typography.labelMedium
                 )
                 SuggestionChip( //TODO: change bg color with days left
                     modifier = Modifier
-                        .padding(horizontal = cardInnerHorizontalPadding)
+                        .padding(horizontal = defaultInnerHorizontalPadding)
                         .padding(top = 8.dp),
                     label = {
                         Text(
@@ -111,7 +109,7 @@ fun WarrantyDetailsScreenContent() {
                 )
                 Text(
                     modifier = Modifier
-                        .padding(horizontal = cardInnerHorizontalPadding)
+                        .padding(horizontal = defaultInnerHorizontalPadding)
                         .padding(bottom = 6.dp),
                     text = "Warranty until 12 Dec 2026",
                     color = darkGrayishCyan,
@@ -120,7 +118,7 @@ fun WarrantyDetailsScreenContent() {
                 LinearProgressIndicator(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = cardInnerHorizontalPadding)
+                        .padding(horizontal = defaultInnerHorizontalPadding)
                         .padding(bottom = 12.dp)
                         .height(6.dp),
                     progress = { 0.35f },
@@ -129,7 +127,126 @@ fun WarrantyDetailsScreenContent() {
                     gapSize = 1.dp
                 )
             }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InfoCardWithTitle(title = "Details") {
+                DoubleStringInfoRow(
+                    icon = ImageVector.vectorResource(R.drawable.ic_store),
+                    firstString = "Store",
+                    secondString = "Rozetka"
+                )
+                PaddingHorizontalDivider()
+                DoubleStringInfoRow(
+                    icon = ImageVector.vectorResource(R.drawable.ic_price),
+                    firstString = "Price",
+                    secondString = "34 999"
+                )
+                PaddingHorizontalDivider()
+                DoubleStringInfoRow(
+                    icon = ImageVector.vectorResource(R.drawable.ic_brand),
+                    firstString = "Brand",
+                    secondString = "Apple"
+                )
+                PaddingHorizontalDivider()
+                DoubleStringInfoRow(
+                    icon = ImageVector.vectorResource(R.drawable.ic_serial_number),
+                    firstString = "Serial Number",
+                    secondString = "C023F32A23B123"
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InfoCardWithTitle(title = "Dates") {
+                DoubleStringInfoRow(
+                    icon = ImageVector.vectorResource(R.drawable.ic_purchase_date),
+                    firstString = "Purchase Date",
+                    secondString = "12 Dec 2025"
+                )
+                PaddingHorizontalDivider()
+                DoubleStringInfoRow(
+                    icon = ImageVector.vectorResource(R.drawable.ic_end_time_hourglass),
+                    firstString = "Expiration Date",
+                    secondString = "12 Dec 2025"
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InfoCardWithTitle(
+                title = "Attachments"
+            ) {
+                //TODO: Show attached photo with card?
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            InfoCardWithTitle(title = "Notes") {
+                Text(
+                    modifier = Modifier.padding(
+                        horizontal = defaultInnerHorizontalPadding,
+                        vertical = 8.dp
+                    ),
+                    text = "Notes notesNotes notesNotes notesNotes notesNotes notesNotes notes"
+                ) //TODO: change style
+            }
+
+            //TODO: Add edit button
         }
+    }
+}
+
+@Composable
+private fun InfoCardWithTitle(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: @Composable () -> Unit
+) {
+    ElevatedCard(
+        modifier = modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerHigh)
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            text = title,
+            style = MaterialTheme.typography.titleMedium
+        )
+        PaddingHorizontalDivider()
+        content()
+    }
+}
+
+@Composable
+private fun DoubleStringInfoRow(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    firstString: String,
+    secondString: String
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            text = firstString,
+            style = MaterialTheme.typography.labelMedium
+        )
+        Text(
+            text = secondString,
+            style = MaterialTheme.typography.labelLarge
+        )
     }
 }
 
