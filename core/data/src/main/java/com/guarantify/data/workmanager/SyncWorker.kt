6be73ade223da6b180827ea98c3d816a2e33f116
Dmytro Dyshuk <dd.kronos@gmail.com>
2026-04-01 17:49:48 +0300

    feat(warranties-sync): implement SyncWorker work logic and error handling

--- core/data/src/main/java/com/guarantify/data/workmanager/SyncWorker.kt
+++ core/data/src/main/java/com/guarantify/data/workmanager/SyncWorker.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.guarantify.domain.model.sync.SyncError
 import com.guarantify.domain.repository.SyncWarrantiesRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import androidx.work.ListenableWorker.Result as WorkResult
+import com.guarantify.common.result.Result as CommonResult
 
 @HiltWorker
 class SyncWorker @AssistedInject constructor(
@@ -15,8 +18,19 @@ class SyncWorker @AssistedInject constructor(
     private val syncWarrantiesRepository: SyncWarrantiesRepository
 ) : CoroutineWorker(appContext, params) {
 
-    override suspend fun doWork(): Result {
-        TODO("Not yet implemented")
+    override suspend fun doWork(): WorkResult {
+        return when (val result = syncWarrantiesRepository.syncWarranties()) {
+            is CommonResult.Success -> WorkResult.success()
+            is CommonResult.Error -> {
+                when (result.throwable) {
+                    is SyncError.DatabaseError -> WorkResult.failure()
+                    is SyncError.NetworkError -> WorkResult.retry()
+                    is SyncError.AuthError -> WorkResult.failure()
+                    is SyncError.UnknownError -> WorkResult.retry()
+                    else -> WorkResult.failure()
+                }
+            }
+        }
     }
 
 }
